feb8888b19ecf9dac9cadc7b584275146aee8c83
Pancakse <84547061+PancakseYT@users.noreply.github.com>
2023-09-20 00:18:34 -0600

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,132 +1,241 @@
-// @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
+import remarkA11yEmoji from "@fec/remark-a11y-emoji";
+import { themes } from "prism-react-renderer";
+import isCI from "is-ci";
+import navbar from "./config/navbar.config";
+import footer from "./config/footer.config";
+import { env } from "process";
+import { Config } from "@docusaurus/types";
+import { Options } from "@docusaurus/plugin-content-docs";
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const preview = env.VERCEL_ENV === "preview";
 
-/** @type {import('@docusaurus/types').Config} */
-const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+const url = (preview && `https://${env.VERCEL_URL}`) || "https://docs.nanaicamc.tk";
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
-
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
-    ],
-  ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'My Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+const docsCommon: Options = {
+  breadcrumbs: true,
+  editUrl: ({ versionDocsDirPath, docPath }) =>
+    `https://github.com/PaperMC/docs/blob/main/${versionDocsDirPath}/${docPath}`,
+  editCurrentVersion: true,
+  remarkPlugins: [remarkA11yEmoji],
+  showLastUpdateAuthor: true,
+  showLastUpdateTime: true,
 };
 
-module.exports = config;
+const config: Config = {
+  title: "NanaicaMC Documentation",
+  tagline:
+    "Documentation for all projects under the NanaicaMC umbrella, including Nanaica, Iron, and Cherry.",
+  url: url,
+  baseUrl: "/",
+  onBrokenLinks: isCI ? "throw" : "warn",
+  onBrokenMarkdownLinks: isCI ? "throw" : "warn",
+  onDuplicateRoutes: isCI ? "throw" : "warn",
+  favicon: "img/favicon.ico",
+  trailingSlash: false,
+  noIndex: preview,
+  baseUrlIssueBanner: false,
+  clientModules: [
+    require.resolve("./src/css/custom.css"),
+    require.resolve("@fontsource/jetbrains-mono/index.css"),
+  ],
+
+  webpack: {
+    jsLoader: (isServer) => ({
+      loader: require.resolve("esbuild-loader"),
+      options: {
+        loader: "tsx",
+        target: isServer ? "node12" : "es2017",
+      },
+    }),
+  },
+
+  headTags: [
+    {
+      tagName: "script",
+      attributes: {
+        type: "application/ld+json",
+      },
+      innerHTML: JSON.stringify({
+        "@context": "https://schema.org",
+        "@type": "WebSite",
+        url,
+        potentialAction: {
+          "@type": "SearchAction",
+          target: {
+            "@type": "EntryPoint",
+            urlTemplate: `${url}/search?q={search_term_string}`,
+          },
+          "query-input": "required name=search_term_string",
+        },
+      }),
+    },
+  ],
+
+  markdown: {
+    mermaid: true,
+    mdx1Compat: {
+      comments: false,
+      admonitions: false,
+      headingIds: false,
+    },
+  },
+
+  themes: ["@docusaurus/theme-classic", "@docusaurus/theme-search-algolia", "@docusaurus/theme-mermaid"],
+
+  plugins: [
+    [
+      "content-docs",
+      {
+        ...docsCommon,
+        id: "misc",
+        path: "docs/misc",
+        routeBasePath: "/misc",
+        sidebarPath: require.resolve("./config/sidebar.misc"),
+      },
+    ],
+    [
+      "content-docs",
+      {
+        ...docsCommon,
+        id: "Nanaica",
+        path: "docs/nanaica",
+        routeBasePath: "nanaica",
+        sidebarPath: require.resolve("./config/sidebar.nanaica"),
+        lastVersion: "current",
+        versions: {
+          current: {
+            label: "1.20",
+            path: "",
+          },
+        },
+      },
+    ],
+    [
+      "content-docs",
+      {
+        ...docsCommon,
+        id: "Plugins",
+        path: "docs/plugins",
+        routeBasePath: "plugins",
+        sidebarPath: require.resolve("./config/sidebar.plugins"),
+        lastVersion: "current",
+        versions: {
+          current: {
+            label: "1.20",
+            path: "",
+          },
+        },
+      },
+    ],
+    [
+      "content-docs",
+      {
+        ...docsCommon,
+        id: "iron",
+        path: "docs/iron",
+        routeBasePath: "iron",
+        sidebarPath: require.resolve("./config/sidebar.iron"),
+      },
+    ],
+    [
+      "content-docs",
+      {
+        ...docsCommon,
+        id: "cherry",
+        path: "docs/cherry",
+        routeBasePath: "cherry",
+        sidebarPath: require.resolve("./config/sidebar.cherry"),
+      },
+    ],
+    [
+      "content-pages",
+      {
+        remarkPlugins: [remarkA11yEmoji],
+      },
+    ],
+    [
+      "pwa",
+      {
+        offlineModeActivationStrategies: ["appInstalled", "standalone", "queryString"],
+        pwaHead: [
+          {
+            tagName: "link",
+            rel: "icon",
+            href: "img/logo.png",
+          },
+          {
+            tagName: "link",
+            rel: "manifest",
+            href: "/manifest.json",
+          },
+          {
+            tagName: "meta",
+            name: "theme-color",
+            content: "rgb(0, 78, 233)",
+          },
+        ],
+      },
+    ],
+    [
+      "@docusaurus/plugin-sitemap",
+      {
+        ignorePatterns: ["**/cat/**"],
+      },
+    ],
+    "@docusaurus/plugin-debug",
+  ],
+
+  themeConfig: {
+    colorMode: {
+      respectPrefersColorScheme: true,
+    },
+    image: "img/logo.png",
+    metadata: [
+      {
+        name: "twitter:card",
+        content: "summary",
+      },
+      {
+        name: "og:type",
+        content: "website",
+      },
+      {
+        name: "og:image:alt",
+        content: "Nanaica Logo",
+      },
+    ],
+    navbar: navbar,
+    footer: footer,
+    docs: {
+      sidebar: {
+        hideable: true,
+      },
+    },
+    prism: {
+      additionalLanguages: [
+        "batch",
+        "bash",
+        "git",
+        "java",
+        "javastacktrace",
+        "kotlin",
+        "groovy",
+        "log",
+        "toml",
+        "properties",
+      ],
+      theme: themes.vsDark,
+    },
+    algolia: {
+      appId: "P1BCDPTG1Q",
+      apiKey: "34772712950f27c6e9c714ad2e6c5e16",
+      indexName: "docs-nanaicamc",
+      contextualSearch: true,
+    },
+    mermaid: {
+      theme: {light: 'neutral', dark: 'dark'},
+    },
+  },
+};
+
+export = config;
